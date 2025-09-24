1799665f3fb143801244bdbb40b840a33677e899
Kalle Björklid <kalle.bjorklid@gmail.com>
2025-09-24 20:12:36 +0300

    Windows install script, wezterm

---
+++ run_once_before_install-windows-stuff.ps1
@@ -0,0 +1,20 @@
+# Self-elevate the script if required
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+    $CommandLine = "-NoExit -File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+    Start-Process -Wait -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+    Exit
+  }
+}
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install -y mingw 
+choco install -y make
+choco install -y unzip
+choco install -y ripgrep
+choco install -y fd
+choco install -y win32yank
+choco install -y wget
+
+
+#Install-Module -Name Terminal-Icons -Repository PSGallery
