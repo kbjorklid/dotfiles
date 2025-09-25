1679656766b1a12ef7164b9fbf25429c83836522
Kalle Björklid <kalle.bjorklid@gmail.com>
2025-09-25 19:01:05 +0300

    Improve the windows script

--- run_once_before_install-windows-stuff.ps1
+++ run_once_before_install-windows-stuff.ps1
@@ -8,13 +8,47 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install -y mingw 
-choco install -y make
-choco install -y unzip
-choco install -y ripgrep
-choco install -y fd
-choco install -y win32yank
-choco install -y wget
+function Install-ChocoPackageIfMissing {
+    param([string]$PackageName)
 
+    $installed = choco list --local-only $PackageName --exact --limit-output
+    if (-not $installed) {
+        Write-Host "Installing $PackageName..."
+        choco install -y $PackageName
+    } else {
+        Write-Host "$PackageName is already installed, upgrading..."
+        choco upgrade -y $PackageName
+    }
+}
+
+function Install-WingetPackage {
+    param([string]$PackageName)
+
+    Write-Host "Installing $PackageName..."
+    winget install $PackageName
+}
+
+Install-WingetPackage "Chocolatey.Chocolatey"
+Install-WingetPackage "Git.Git"
+Install-WingetPackage "Neovim.Neovim"
+Install-WingetPackage "Google.Chrome"
+Install-WingetPackage "GitHub.cli"
+Install-WingetPackage "Google.GoogleDrive"
+Install-WingetPackage "Microsoft.PowerShell"
+Install-WingetPackage "wez.wezterm"
+Install-WingetPackage "ajeetdsouza.zoxide"
+Install-WingetPackage "JesseDuffield.lazygit"
+Install-WingetPackage "Microsoft.PowerToys"
+Install-WingetPackage "Microsoft.WindowsTerminal"
+Install-WingetPackage "Discord.Discord"
+Install-WingetPackage "OpenJS.NodeJS.LTS"
+
+Install-ChocoPackageIfMissing "mingw"
+Install-ChocoPackageIfMissing "make"
+Install-ChocoPackageIfMissing "unzip"
+Install-ChocoPackageIfMissing "ripgrep"
+Install-ChocoPackageIfMissing "fd"
+Install-ChocoPackageIfMissing "win32yank"
+Install-ChocoPackageIfMissing "wget"
 
 #Install-Module -Name Terminal-Icons -Repository PSGallery
